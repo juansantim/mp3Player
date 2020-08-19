07c2af4f10e5e607d527d8154ee864b0c1771ffe
JuanSantiThinkPad <valen500@gmail.com>
2020-08-19 11:21:50 -0400

    ultimos cambios de usuaro a admin 123

--- src/Model/model.js
+++ src/Model/model.js
@@ -33,11 +33,11 @@ model.Usuarios = connection.define('Users', {
 model.Usuarios.count().then(count => {
   if(count == 0){
     model.Usuarios.create({
-      UserName: 'jsanti',
+      UserName: 'admin',
       Password: '202cb962ac59075b964b07152d234b70',
-      Email: 'juanv.santim@gmail.com',
-      FirstName: 'Juan',
-      LastName: 'Santi'
+      Email: 'test@gmail.com',
+      FirstName: 'Admin',
+      LastName: 'Root'
     })
   }
 })
